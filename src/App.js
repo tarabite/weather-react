4b0ad3cc0ff1b5a93660e2815d99b377ff196423
tarabite <88362366+tarabite@users.noreply.github.com>
2021-12-19 15:46:25 -0800

    Linked Git Repository

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello World!</h1>
+        <p>
+          To Start or Stop the Terminial server:
+          <br />
+          Type "npm start"
+          <br />
+          To stop the server, go to terminal, select node and type "ctrl+c"
+        </p>
+        <Weather />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
