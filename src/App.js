7311d1633ea6a7bd8f11e2d33cfd08ba8db3d1c0
tarabite <88362366+tarabite@users.noreply.github.com>
2021-12-19 16:38:45 -0800

    Copied Weather search from old assignment

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from "react";
 import Weather from "./Weather";
 import logo from "./logo.svg";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-        <p>
-          To Start or Stop the Terminial server:
-          <br />
-          Type "npm start"
-          <br />
-          To stop the server, go to terminal, select node and type "ctrl+c"
-        </p>
-        <Weather city="Paris" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Weather App</h1>
+
+        <Weather />
       </header>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -1,15 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
-export default function Weather(props) {
-  function handleResponse(response) {
-    alert(`The weather in ${props.city} is ${response.data.main.temp}°C`);
+export default function Weather() {
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState("");
+
+  function showConditions(response) {
+    setLoaded(true);
+
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}.png`,
+    });
   }
 
-  let apiKey = "93f3a8747a9ff1ebceb2f52d70ba80cb";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}units=metric`;
+  function handleSubmit(event) {
+    event.preventDefault();
+    let apiKey = "93f3a8747a9ff1ebceb2f52d70ba80cb";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showConditions);
+  }
 
-  axios.get(apiUrl).then(handleResponse);
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
 
-  return <h2>Hello from Weather App</h2>;
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="search"
+        placeholder="Enter a city..."
+        onChange={updateCity}
+      />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <ul>
+          <li>Temperature: {Math.round(weather.temperature)}℃</li>
+          <li>Description: {weather.description}</li>
+          <li>Wind: {Math.round(weather.wind)}km/h</li>
+          <li>Humidity: {weather.humidity}%</li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
 }
